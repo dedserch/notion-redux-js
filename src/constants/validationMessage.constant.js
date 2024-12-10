9a1e35cbe25fca19e4eb00d70b0f68243d1995f2
dedserch <dedmemchik@gmail.com>
2024-12-10 22:08:58 +0300

    Change validation message

--- src/constants/validationMessage.constant.js
+++ src/constants/validationMessage.constant.js
@@ -1,6 +1,7 @@
 export const VALIDATION_MESSAGES = {
   INVALID_EMAIL: "Invalid email address.",
-  INVALID_PASSWORD: "Password must be at least 6 characters long.",
+  INVALID_PASSWORD:
+    "Password must be at least 6 characters long, contain an uppercase letter, a lowercase letter, and a number.",
   PASSWORDS_DO_NOT_MATCH: "Passwords do not match.",
   REGISTRATION_FAILED: "Registration failed. Please try again.",
   LOGIN_FAILED: "Login failed. Please check your credentials.",
